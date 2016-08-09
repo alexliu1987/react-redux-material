24cd4ad3c50c9673f92748d3ff138e54c16cca7b
alexliu <alex.liu1987@foxmail.com>
2016-08-09 17:24:54 +0800

    update promise

--- src/actions/menu.js
+++ src/actions/menu.js
@@ -6,6 +6,7 @@ import {
 	cac
 } from '../utils'
 import axios from 'axios'
+import config from 'config'
 
 export const setMenuIsOpen = cac(SET_MENU_ISOPEN, 'value')
 const receiveMenuList = cac(RECEIVE_MENU_LIST, 'data')
@@ -13,7 +14,7 @@ const receiveMenuList = cac(RECEIVE_MENU_LIST, 'data')
 export function fetchMenu() {
 	return (dispatch) => {
 		axios.request({
-				url: '/data/menu.json',
+				url: config.api + '/menu',
 				responseType: 'json'
 			})
 			.then((data) => {
--- src/config/test.js
+++ src/config/test.js
@@ -3,7 +3,8 @@
 import baseConfig from './base';
 
 let config = {
-  appEnv: 'test'  // don't remove the appEnv property here
+	appEnv: 'test', // don't remove the appEnv property here
+	api: 'http://localhost:20001/api'
 };
 
 export default Object.freeze(Object.assign(baseConfig, config));
